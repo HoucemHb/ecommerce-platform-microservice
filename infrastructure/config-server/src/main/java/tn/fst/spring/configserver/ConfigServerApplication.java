5d49787eb45a779026faadbe1f542ab1bac768d7
Houcem Hbiri <houcem96.hh@gmail.com>
2026-01-18 14:30:30 +0100

    config server + eureka

--- infrastructure/config-server/src/main/java/tn/fst/spring/configserver/ConfigServerApplication.java
+++ infrastructure/config-server/src/main/java/tn/fst/spring/configserver/ConfigServerApplication.java
@@ -1,13 +1,13 @@
-package tn.fst.spring.apigateway;
+package tn.fst.spring.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer  // Enable Config Server
 public class ConfigServerApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
-
 }
